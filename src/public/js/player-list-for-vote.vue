01010a6c6dca1f6d9d4a3c93be6f22b4e6187148
Mike Beck <mike@mikeybeck.com>
2022-04-02 18:46:05 +1300

    feat: Prevent users voting for themselves

--- src/public/js/player-list-for-vote.vue
+++ src/public/js/player-list-for-vote.vue
@@ -1,9 +1,9 @@
 <template>
 	<select ref="selectRef" class="player-list-for-vote" :required="true" @change="changeVote">
 		<option :selected="true">Select a player</option>
-		<option v-for="option in users" v-bind:key="'0' + option.name" v-bind:value="option.name">{{
-			option.name
-		}}</option>
+		<option v-for="option in getUsers" v-bind:key="'0' + option.name" v-bind:value="option.name">
+			{{ option.name }}
+		</option>
 	</select>
 </template>
 
@@ -19,6 +19,16 @@ export default {
 			type: String,
 		},
 	},
+    data() {
+        return {
+            state: Store.state,
+        }
+    },
+    computed: {
+        getUsers() {
+            return this.users.filter(user => user.name !== this.state.username);
+        },
+    },
 	watch: {
 		votedUsername: function(newVal, oldVal) {
 			this.$refs.selectRef.value = newVal;
